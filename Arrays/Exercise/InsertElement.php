4d8ae13fd89779a4f5c474bcfa13c37d3ff36481
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2024-05-25 23:40:32 +0700

    Update Exercise By A Ty

---
+++ Arrays/Exercise/InsertElement.php
@@ -0,0 +1,25 @@
+<?php
+
+class InsertElement {
+
+    public function insertElement(array $arr, int $pos, $value): array
+    {
+        $n = sizeof($arr);
+
+        for ($i = $n - 1; $i >= $pos; $i--) {
+            $arr[$i + 1] = $arr[$i];
+        }
+
+        $arr[$pos] = $value;
+
+        return $arr;
+    }
+
+}
+
+$arr = [1,2,3,4,5];
+$pos = 2;
+$value = 99;
+$insertElement = new InsertElement();
+print_r($insertElement->insertElement($arr, $pos, $value));
+

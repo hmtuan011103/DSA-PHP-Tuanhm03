231e9ca40f4e0550802102765f5d929b6315524e
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2024-06-05 18:24:51 +0700

    Linked List Basic insert

--- LinkedList/Single/BasicOperations/BasicLinkedList.php
+++ LinkedList/Single/BasicOperations/BasicLinkedList.php
@@ -6,14 +6,14 @@ require '../Node.php';
 
 class BasicLinkedList
 {
-    private $head;
+    public $head;
 
     public function __construct()
     {
         $this->head = null;
     }
 
-    public function insert($data): void
+    public function insert($data): ?Node
     {
         $newNode = new Node($data);
 
@@ -26,28 +26,75 @@ class BasicLinkedList
             }
             $current->next = $newNode;
         }
+
+        return $this->head;
     }
 
     /**
+     * Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+     * Return the linked list sorted as well.
+     *
+     * Input: head = [1,1,2]
+     * Output: [1,2]
+     *
+     * Input: head = [1,1,2,3,3]
+     * Output: [1,2,3]
+     *
+     * Constraints
+     * The number of nodes in the list is in the range [0, 300].
+     * -100 <= Node.val <= 100
+     * The list is guaranteed to be sorted in ascending order.
      * @return Node|null
      */
     public function deleteDuplicates(): ?Node
     {
+        $prev = $this->head;
         $current = $this->head->next;
-        $prev    = $this->head;
 
         while ($current !== null) {
             if ($prev->data === $current->data) {
-                $temp       = $current->next;
+                $temp = $current->next;
                 $prev->next = $temp;
-                $current    = $temp;
+                $current = $temp;
             } else {
-                $prev    = $current;
+                $prev = $current;
                 $current = $current->next;
             }
-
         }
+
         return $this->head;
+    }
+
+    public function insertToBegin($data): void
+    {
+        $firstNode = new Node($data);
+        $firstNode->next = $this->head;
+        $this->head = $firstNode;
+    }
+
+    public function insertAfter($prevNode, $data): int
+    {
+        if ($prevNode === null) {
+            return -1;
+        }
+        $newNode = new Node($data);
+        $newNode->next = $prevNode->next;
+        $prevNode->next = $newNode;
+        return 1;
+    }
+
+    public function getNodeByValue($data): Node|int
+    {
+        $current = $this->head;
+
+        while ($current !== null) {
+            if ($current->data === $data) {
+                return $current;
+            }
+            $current = $current->next;
+        }
+
+        return -1;
     }
 
     public function traverse(): void
@@ -69,8 +116,10 @@ $basicLinkedList->insert(2);
 $basicLinkedList->insert(3);
 $basicLinkedList->insert(3);
 
+//$nodeByValue = $basicLinkedList->getNodeByValue(3);
+//$basicLinkedList->insertAfter($nodeByValue, 999);
+
 $newLinkedList = $basicLinkedList->deleteDuplicates();
-print_r($newLinkedList);
 $basicLinkedList->traverse();
 
 

44ce2385d633fc9f70d42fe78407018361a5c228
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2024-06-08 23:56:11 +0700

    Solution Lesson 1 removeElements

--- LinkedList/Single/BasicOperations/BasicLinkedList.php
+++ LinkedList/Single/BasicOperations/BasicLinkedList.php
@@ -40,24 +40,22 @@ class BasicLinkedList
      * Input: head = [1,1,2,3,3]
      * Output: [1,2,3]
      *
-     * Constraints
-     * The number of nodes in the list is in the range [0, 300].
-     * -100 <= Node.val <= 100
+     * Constraints:
      * The list is guaranteed to be sorted in ascending order.
      * @return Node|null
      */
     public function deleteDuplicates(): ?Node
     {
-        $prev = $this->head;
+        $prev    = $this->head;
         $current = $this->head->next;
 
         while ($current !== null) {
             if ($prev->data === $current->data) {
-                $temp = $current->next;
+                $temp       = $current->next;
                 $prev->next = $temp;
-                $current = $temp;
+                $current    = $temp;
             } else {
-                $prev = $current;
+                $prev    = $current;
                 $current = $current->next;
             }
         }
@@ -67,9 +65,9 @@ class BasicLinkedList
 
     public function insertToBegin($data): void
     {
-        $firstNode = new Node($data);
+        $firstNode       = new Node($data);
         $firstNode->next = $this->head;
-        $this->head = $firstNode;
+        $this->head      = $firstNode;
     }
 
     public function insertAfter($prevNode, $data): int
@@ -77,8 +75,8 @@ class BasicLinkedList
         if ($prevNode === null) {
             return -1;
         }
-        $newNode = new Node($data);
-        $newNode->next = $prevNode->next;
+        $newNode        = new Node($data);
+        $newNode->next  = $prevNode->next;
         $prevNode->next = $newNode;
         return 1;
     }
@@ -106,21 +104,54 @@ class BasicLinkedList
             $current = $current->next;
         }
     }
+
+    public function removeElements($head, $val) {
+        if ($head === null) {
+            return null;
+        }
+
+        $head->next = $this->removeElements($head->next, $val);
+
+        if ($head->data === $val) {
+            return $head->next;
+        }
+
+        return $head;
+
+
+//        $dummy = new ListNode(0);
+//        $dummy->next = $head;
+//        $current = $dummy;
+//
+//        while ($current->next !== null) {
+//            if ($current->next->val === $val) {
+//                $current->next = $current->next->next;
+//            } else {
+//                $current = $current->next;
+//            }
+//        }
+//
+//        return $dummy->next;
+    }
+
 }
 
 $basicLinkedList = new BasicLinkedList();
 
 $basicLinkedList->insert(1);
-$basicLinkedList->insert(1);
 $basicLinkedList->insert(2);
+$basicLinkedList->insert(6);
 $basicLinkedList->insert(3);
-$basicLinkedList->insert(3);
+$basicLinkedList->insert(4);
+$basicLinkedList->insert(5);
+$basicLinkedList->insert(6);
 
-$nodeByValue = $basicLinkedList->getNodeByValue(3);
-//$basicLinkedList->insertAfter($nodeByValue, 999);
+//$nodeByValue = $basicLinkedList->getNodeByValue(3);
+$data = $basicLinkedList->head;
+$basicLinkedList->removeElements($data, 6);
 
-$newLinkedList = $basicLinkedList->deleteDuplicates();
-$basicLinkedList->traverse();
+//$newLinkedList = $basicLinkedList->deleteDuplicates();
+//$basicLinkedList->traverse();
 
 
 

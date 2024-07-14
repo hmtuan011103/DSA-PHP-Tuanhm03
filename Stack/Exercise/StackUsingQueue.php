8bc4a1197139400f5274d60b060ecdc27b484341
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2024-07-14 12:21:46 +0700

    Stack using queue

---
+++ Stack/Exercise/StackUsingQueue.php
@@ -0,0 +1,59 @@
+<?php
+
+class MyStack
+{
+    private array $queue1;
+    private array $queue2;
+
+    public function __construct()
+    {
+        $this->queue1 = [];
+        $this->queue2 = [];
+    }
+
+    public function push($x): void
+    {
+        $this->queue1[] = $x;
+    }
+
+    public function pop(): int
+    {
+        while (count($this->queue1) > 1) {
+            $this->queue2[] = array_shift($this->queue1);
+        }
+        $res = array_shift($this->queue1);
+
+        $this->queue1 = $this->queue2;
+        $this->queue2 = [];
+
+        return $res;
+    }
+
+    public function top()
+    {
+        while (count($this->queue1) > 1) {
+            $this->queue2[] = array_shift($this->queue1);
+        }
+        $res            = array_shift($this->queue1);
+        $this->queue2[] = $res;
+
+        $this->queue1 = $this->queue2;
+        $this->queue2 = [];
+
+        return $res;
+    }
+
+    public function empty(): bool
+    {
+        return empty($this->queue1);
+    }
+}
+
+$stack = new MyStack();
+$stack->push(1);
+$stack->push(2);
+
+echo $stack->top() . "\n"; // Output: 2
+echo $stack->pop() . "\n"; // Output: 2
+
+echo $stack->empty() ? 'true' : 'false'; // Output: false
